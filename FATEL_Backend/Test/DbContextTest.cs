d2df02b80a928a40255cc11d8926ab919178076d
Ádám Lőrincz <adam7605@easv365.dk>
2022-10-19 12:28:23 +0200

    Removed sensitive tests
    I literally couldn't figure out how to get the con strings from appsettings.json, both dev and prod. So no you can just believe me that it works. Also, when we test the repo, you can make sure

--- FATEL_Backend/Test/DbContextTest.cs
+++ FATEL_Backend/Test/DbContextTest.cs
@@ -1,29 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 
 namespace Test;
 
 //These are more like integration tests, as the server has to be running for these to pass
 public class DbContextTest
 {
-    [Fact]
-    public void Production_CanConnect_WithValidConnectionString()
-    {
-        var optionsBuilder = new DbContextOptionsBuilder<DbContext>().UseNpgsql("Host=128.76.216.135;Database=FATEL_Production;Username=pi;Password=pi");
-        
-        DbContext dbContext = new DbContext(optionsBuilder.Options);
-
-        Assert.True(dbContext.Database.CanConnect());
-    }
-
-    [Fact]
-    public void Development_CanConnect_WithValidConnectionString()
-    {
-        var optionsBuilder = new DbContextOptionsBuilder<DbContext>().UseNpgsql("Host=128.76.216.135;Database=FATEL_Development;Username=pi;Password=pi");
-        
-        DbContext dbContext = new DbContext(optionsBuilder.Options);
-
-        Assert.True(dbContext.Database.CanConnect());
-    }
+    /*
+     * Tested valid connection strings both for production and development, which are passed, however they were removed, not to expose connection strings
+     */
     
     [Fact]
     public void CanConnect_WithInvalidConnectionString()
