d701effd7507768dc6a11aac5b290acdd2d5d744
Ádám Lőrincz <90683098+Ladam0203@users.noreply.github.com>
2022-10-31 13:33:55 +0100

    Update FATEL_Backend/Application/Interfaces/IItemService.cs
    Co-authored-by: Romčo <90683512+RomanMeasv@users.noreply.github.com>

--- FATEL_Backend/Application/Interfaces/IItemService.cs
+++ FATEL_Backend/Application/Interfaces/IItemService.cs
@@ -8,6 +8,6 @@ public interface IItemService
     public Item Create(PostItemDTO postClientDto); //Application layer will parse this into an Item
     public Item Read(int id);
     public List<Item> ReadAll();
-    public Item Update(string id, Item item); //To be able to check whether the body and header corresponds
+    public Item Update(int id, Item item); //To be able to check whether the body and header corresponds
     public Item Delete(int id);
 }
