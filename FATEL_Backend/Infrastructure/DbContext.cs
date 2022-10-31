67bfeb896e7ffab84ea2bfe22e408e30f40c46d5
Ádám Lőrincz <adam7605@easv365.dk>
2022-10-31 12:33:36 +0100

    Added tests for ItemRepository and ItemService

--- FATEL_Backend/Infrastructure/DbContext.cs
+++ FATEL_Backend/Infrastructure/DbContext.cs
@@ -7,6 +7,11 @@ public class DbContext : Microsoft.EntityFrameworkCore.DbContext
     public DbContext(DbContextOptions<DbContext> options) : base(options)
     {
     }
+
+    public DbContext() //solely for mocking purposes
+    {
+        
+    }
     
     //TODO: OnModelCreating
     //TODO: Table mapping for entities
--- FATEL_Backend/Test/ItemRepositoryTest.cs
+++ FATEL_Backend/Test/ItemRepositoryTest.cs
@@ -1,6 +1,38 @@
+using Application;
+using Application.Interfaces;
+using Infrastructure;
+using Moq;
+
 namespace Test;
 
 public class ItemRepositoryTest
 {
+    [Fact]
+    public void CreateItemRepository_WithNullDbContext_ExpectArgumentNullException()
+    {
+        //Arrange
+
+        //Act
+        IItemRepository itemRepository = null;
+        var e = Assert.Throws<ArgumentNullException>(() => itemRepository = new ItemRepository(null));
+        
+        //Assert
+        Assert.Equal("Value cannot be null. (Parameter 'context')", e.Message);
+        Assert.Null(itemRepository);
+    }
     
+    [Fact]
+    public void CreateItemRepository_WithDbContext()
+    {
+        //Arrange
+        var mockDbContext = new Mock<DbContext>();
+        
+        //Act
+        IItemRepository itemRepository = new ItemRepository(mockDbContext.Object);
+        
+        //Assert
+        Assert.NotNull(itemRepository);
+        Assert.True(itemRepository is ItemRepository);
+        //TODO: Test if the repository is truly injected
+    }
 }
