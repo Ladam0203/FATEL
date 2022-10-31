f9b9ab8dac4506846266fb173b89eb4107cdebe7
Miloš Jozek <90444097+BoldHeadBall@users.noreply.github.com>
2022-10-31 16:41:44 +0100

    FluentValidation, DTOs, Item Controller

---
+++ FATEL_Backend/Application/Validators/PostItemDTOValidator.cs
@@ -0,0 +1,19 @@
+﻿using Application.DTOs;
+using Domain;
+using FluentValidation;
+
+namespace Application.Validators;
+
+public class PostItemDTOValidator : AbstractValidator<PostItemDTO>
+{
+    public PostItemDTOValidator()
+    {
+        RuleFor(item => item.Name).NotEmpty();
+        RuleFor(item => item.Lenght).GreaterThan(0);
+        RuleFor(item => item.Width).GreaterThan(0);
+        //ask alex about if statement in fluent valid. 
+        //example => if unit == pieces, quantity greaterThan 0
+        RuleFor(item => item.Lenght).GreaterThan(0).When(item => item.Unit == Unit.Meter);
+    }
+
+}
