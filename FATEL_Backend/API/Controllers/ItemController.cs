5823ced167714c4a2ebc64d0fdc2f4620552f89b
Miloš Jozek <90444097+BoldHeadBall@users.noreply.github.com>
2022-10-31 16:41:44 +0100

    FluentValidation, DTOs, Item Controller

---
+++ FATEL_Backend/API/Controllers/ItemController.cs
@@ -0,0 +1,40 @@
+﻿using Application;
+using Application.DTOs;
+using Application.Validators;
+using AutoMapper;
+using Domain;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+
+[ApiController]
+[Route("[Controller]")]
+public class ItemController : ControllerBase
+{
+    private ItemRepository _itemRepository;
+    private ItemValidator _itemValidator;
+    private IMapper _mapper;
+
+    public ItemController(ItemRepository itemRepository, IMapper mapper)
+    {
+        _itemRepository = itemRepository;
+        _itemValidator = new ItemValidator();
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public ActionResult CreateItem(PostItemDTO itemDto)
+    {
+        var validate = _itemValidator.Validate(itemDto);
+        if (!validate.IsValid) 
+            return BadRequest(validate.ToString());
+        var newItem = _mapper.Map<Item>(itemDto);
+        return Ok(_itemRepository.Create(newItem));
+
+    }
+
+
+
+}
--- FATEL_Backend/Application/DTOs/PostItemDTO.cs
+++ FATEL_Backend/Application/DTOs/PostItemDTO.cs
@@ -1,6 +1,14 @@
+using Domain;
+
 namespace Application.DTOs;
 
 public class PostItemDTO
 {
-    
+
+    public string Name { get; set; }
+    public float?  Width { get; set; }
+    public float? Lenght { get; set; }
+    public Unit Unit { get; set; }
+    public int Quantity { get; set; }
+    public string? Note { get; set; }
 }
