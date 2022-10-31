44c9fcc745208a259f4f56dc3ac4b3b2f409631a
Ádám Lőrincz <adam7605@easv365.dk>
2022-10-31 14:57:54 +0100

    Added implementation and test

--- FATEL_Backend/Test/ItemServiceTest.cs
+++ FATEL_Backend/Test/ItemServiceTest.cs
@@ -1,5 +1,6 @@
 using Application;
 using Application.Interfaces;
+using Domain;
 using Moq;
 
 namespace Test;
@@ -33,5 +34,46 @@ public class ItemServiceTest
         Assert.NotNull(itemService);
         Assert.True(itemService is ItemService);
         //TODO: Test if the repository is truly injected
+    }
+
+    [Fact]
+    public void Read()
+    {
+        //Arrange
+        var mockRepository = new Mock<IItemRepository>();
+        int mockId = 0;
+        Item mockItem = new Item(); //TODO: Use a detailed item and test with that
+        mockRepository.Setup(r => r.Read(mockId)).Returns(mockItem);
+        
+        IItemService itemService = new ItemService(mockRepository.Object);
+        
+        //Act
+        Item readItem = itemService.Read(mockId);
+
+        //Assert
+        Assert.NotNull(readItem);
+        Assert.True(readItem is Item);
+        Assert.Equal(mockItem, readItem);
+        mockRepository.Verify(r => r.Read(mockId), Times.Once);
+    }
+    
+    [Fact]
+    public void ReadAll()
+    {
+        //Arrange
+        var mockRepository = new Mock<IItemRepository>();
+        List<Item> mockItems = new List<Item>(); //TODO: Use detailed items and test with that
+        mockRepository.Setup(r => r.ReadAll()).Returns(mockItems);
+        
+        IItemService itemService = new ItemService(mockRepository.Object);
+        
+        //Act
+        List<Item> readItems = itemService.ReadAll();
+
+        //Assert
+        Assert.NotNull(readItems);
+        Assert.True(readItems is List<Item>);
+        Assert.Equal(mockItems, readItems);
+        mockRepository.Verify(r => r.ReadAll(), Times.Once);
     }
 }
