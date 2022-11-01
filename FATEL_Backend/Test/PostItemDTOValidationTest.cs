78d41d71990a04f7cd1df87d1800a6e3d7653a61
Miloš Jozek <90444097+BoldHeadBall@users.noreply.github.com>
2022-11-01 11:09:41 +0100

    Finished postItemDTOValidator and first test for it

---
+++ FATEL_Backend/Test/PostItemDTOValidationTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Intrinsics;
+using Application.DTOs;
+using Application.Validators;
+using Domain;
+
+namespace Test;
+
+public class PostItemDTOValidationTest
+{
+    [Fact]
+    public void ValidatePostItemDTO_WithAllValidFields()
+    {
+        //Arrange
+        PostItemDTO postItemDTO = new PostItemDTO() { Name = "Plank", Lenght = 2, Note = "This is a plank", Unit = Unit.Meter };
+        var validator = new PostItemDTOValidator();
+        //Act
+        var e = validator.Validate(postItemDTO);
+        //Assert
+        Assert.True(e.IsValid);
+    }
+    
+    
+}
