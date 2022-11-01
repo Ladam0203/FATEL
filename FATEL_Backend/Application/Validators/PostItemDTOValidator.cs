5d1f0da15d1d42e7cebf10fe543b48413c5335ae
Ádám Lőrincz <adam7605@easv365.dk>
2022-11-01 13:38:02 +0100

    Tested and corrected the PostItemDTO Validator

--- FATEL_Backend/Application/Validators/PostItemDTOValidator.cs
+++ FATEL_Backend/Application/Validators/PostItemDTOValidator.cs
@@ -8,21 +8,42 @@ public class PostItemDTOValidator : AbstractValidator<PostItemDTO>
 {
     public PostItemDTOValidator()
     {
-        RuleFor(item => item.Name).NotEmpty();
-        RuleFor(item => item.Unit).NotEmpty();
-
-        //Quantity
-        RuleFor(item => item.Quantity).GreaterThan(0);
+        
+        //Name
+        RuleFor(item => item.Name)
+            .NotEmpty().WithMessage("Name must not be empty");
+        //Unit
+        RuleFor(item => item.Unit)
+            .NotNull()
+            .IsInEnum().WithMessage("Unit must not be a valid enum value");
+        
         //Piece Unit
-        RuleFor(item => item.Length).Empty().When(item => item.Unit == Unit.Piece);
-        RuleFor(item => item.Width).Empty().When(item => item.Unit == Unit.Piece);
+        RuleFor(item => item.Length)
+            .Null().When(item => item.Unit == Unit.Piece).WithMessage("Length must be null if the unit is Piece");
+        RuleFor(item => item.Width)
+            .Null().When(item => item.Unit == Unit.Piece).WithMessage("Width must be null if the unit is Piece");
         //Meter Unit
-        RuleFor(item => item.Length).GreaterThan(0).When(item => item.Unit == Unit.Meter);
-        RuleFor(item => item.Width).Empty().When(item => item.Unit == Unit.Meter);
+        RuleFor(item => item.Length)
+            .NotNull().When(item => item.Unit == Unit.Meter).WithMessage("Length must not be null if the unit is Meter");
+        RuleFor(item => item.Length)
+            .GreaterThan(0).When(item => item.Unit == Unit.Meter).WithMessage("Length has to be greater than 0 if the unit is Meter");
+        RuleFor(item => item.Width)
+            .Null().When(item => item.Unit == Unit.Meter).WithMessage("Width must be null if the unit is Meter");
         //SquareMeter Unit
-        RuleFor(item => item.Length).GreaterThan(0).When(item => item.Unit == Unit.SquareMeter);
-        RuleFor(item => item.Width).GreaterThan(0).When(item => item.Unit == Unit.SquareMeter);
-        //if note is not null then note cannot be empty
-        RuleFor(item => item.Note).NotEmpty().When(item => item.Note != null);
+        RuleFor(item => item.Length)
+            .NotNull().When(item => item.Unit == Unit.SquareMeter).WithMessage("Length must not be null if the unit is SquareMeter");
+        RuleFor(item => item.Length)
+            .GreaterThan(0).When(item => item.Unit == Unit.SquareMeter).WithMessage("Length has to be greater than 0 if the unit is SquareMeter");
+        RuleFor(item => item.Width)
+            .NotNull().When(item => item.Unit == Unit.SquareMeter).WithMessage("Width must not be null if the unit is SquareMeter");
+        RuleFor(item => item.Width)
+            .GreaterThan(0).When(item => item.Unit == Unit.SquareMeter).WithMessage("Width has to be greater than 0 if the unit is SquareMeter");
+        //Quantity
+        RuleFor(item => item.Quantity)
+            .NotNull()
+            .GreaterThanOrEqualTo(0).WithMessage("Quantity has to be greater than or equal to 0");
+        //Note
+        RuleFor(item => item.Note)
+            .NotEmpty().When(item => item.Note != null).WithMessage("Note must not be empty");
     }
 }
--- FATEL_Backend/Test/PostItemDTOValidationTest.cs
+++ FATEL_Backend/Test/PostItemDTOValidationTest.cs
@@ -8,48 +8,38 @@ namespace Test;
 
 public class PostItemDTOValidationTest
 {
-    [Fact]
-    public void ValidatePostItemDTO_WithAllValidFields()
-    {
-        //Arrange
-        PostItemDTO postItemDTO = new PostItemDTO() { Name = "Plank", Length = 2, Note = "This is a plank", Unit = Unit.Meter };
-        var validator = new PostItemDTOValidator();
-        //Act
-        var e = validator.Validate(postItemDTO);
-        //Assert
-        Assert.True(e.IsValid);
-    }
-    
     [Theory]
-    //Testing null/empty values at inappropriate places
-    [InlineData(null, 2, 2, Unit.SquareMeter, 2, "This is a plank", false)]
-    [InlineData("", 2, 2, Unit.SquareMeter, 2, "This is a plank", false)]
-    [InlineData("Plank", 2, 2, null, 2, "This is a plank", false)]
-    [InlineData("Plank", 2, 2, Unit.SquareMeter, null, "This is a plank", false)]
+    //Testing null/empty values at inappropriate places, not regarding length and width7
+    //Name
+    [InlineData(null, null, null, Unit.Piece, 2, "Valid note", false, "Name must not be empty")]
+    [InlineData("", null, null, Unit.Piece, 2, "Valid note", false, "Name must not be empty")]
+    //Unit
+    //Unit cannot be null, programatically TODO: Ask Bent
     //Note is okay to be null, but not empty
-    [InlineData("plank", 2, 2, Unit.SquareMeter, 2, "", false)]
-    [InlineData("plank", 2, 2, Unit.SquareMeter, 2, null, true)]
+    [InlineData("Valid name", null, null, Unit.Piece, 2, null, true, "")]
+    [InlineData("Valid name", null, null, Unit.Piece, 2, "", false, "Note must not be empty")]
     //Testing invalid numbers
-    [InlineData("Plank", -2, 2, Unit.SquareMeter, 2, "This is a plank", false)]
-    [InlineData("Plank", 2, -2, Unit.SquareMeter, 2, "This is a plank", false)]
-    [InlineData("Plank", 2, 2, Unit.SquareMeter, -2, "This is a plank", false)]
-    [InlineData("Plank", -2, -2, Unit.SquareMeter, -2, "This is a plank", false)]
+    [InlineData("Valid name", null, null, Unit.Piece, -2, "Valid note", false, "Quantity has to be greater than or equal to 0")]
+    [InlineData("Valid name", -2, null, Unit.Meter, 2, "Valid note", false, "Length has to be greater than 0 if the unit is Meter")]
+    [InlineData("Valid name", 2, -3, Unit.SquareMeter, 2, "Valid note", false, "Width has to be greater than 0 if the unit is SquareMeter")]
     //Testing the "width/lenght based on unit problem" - Happy cases
-    [InlineData("Plank", null, 2, Unit.Meter, 2, "This is a plank", true)]
-    [InlineData("Plank", 2, 2, Unit.SquareMeter, 2, "This is a plank", true)]
-    [InlineData("Plank", null, null, Unit.Piece, 2, "This is a plank", true)]
-    //Testing the "width/lenght based on unit problem" - Sad cases
-    [InlineData("Plank", 2, null, Unit.Meter, 2, "This is a plank", false)]
-    [InlineData("Plank", 0, 2, Unit.SquareMeter, 2, "This is a plank", false)]
-    [InlineData("Plank", 2, 2, Unit.Piece, 2, "This is a plank", false)]
-    public void ValidatePostItemDTO(string name, float width, float length, Unit unit, int quantity, string note, bool expected)
+    [InlineData("Valid name", null, null, Unit.Piece, 2, "Valid note", true, "")]
+    [InlineData("Valid name", 2, null, Unit.Meter, 2, "Valid note", true, "")]
+    [InlineData("Valid name", 2, 3, Unit.SquareMeter, 2, "Valid note", true, "")]
+    //Testing the "width/lenght based on unit problem" - Sad cases, null values
+    [InlineData("Valid name", 1, null, Unit.Piece, 2, "Valid note", false, "Length must be null if the unit is Piece")]
+    [InlineData("Valid name", 1, 3, Unit.Meter, 2, "Valid note", false, "Width must be null if the unit is Meter")]
+    [InlineData("Valid name", null, 3, Unit.SquareMeter, 2, "Valid note", false, "Length must not be null if the unit is SquareMeter")]
+    //Testing the "width/lenght based on unit problem" - Sad cases, invalid values
+    [InlineData("Valid name", 2, -3, Unit.SquareMeter, 2, "Valid note", false, "Width has to be greater than 0 if the unit is SquareMeter")]
+    public void ValidatePostItemDTO(string name, float? length, float? width, Unit unit, int quantity, string note, bool expected, string expectedMessage)
     {
         //Arrange
         PostItemDTO postItemDTO = new()
         {
             Name = name,
-            Width = width,
             Length = length,
+            Width = width,
             Unit = unit,
             Quantity = quantity,
             Note = note
@@ -57,8 +47,11 @@ public class PostItemDTOValidationTest
         
         var validator = new PostItemDTOValidator();
         //Act
-        var result = validator.Validate(postItemDTO).IsValid;
+        var result = validator.Validate(postItemDTO);
+        var resultIsValid = result.IsValid;
+        var resultMessage = result.ToString();
         //Assert
-        Assert.Equal(expected, result);
+        Assert.Equal(expectedMessage, resultMessage);
+        Assert.Equal(expected, resultIsValid);
     }
 }
