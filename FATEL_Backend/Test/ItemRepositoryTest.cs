745af44343c07ca89fe9a81ebd67740ba661b00a
ChristianHesberg <chri59kx@easv365.dk>
2022-11-01 10:20:34 +0100

    Item delete implemented in repo up to controller with tests

--- FATEL_Backend/Test/ItemRepositoryTest.cs
+++ FATEL_Backend/Test/ItemRepositoryTest.cs
@@ -64,22 +64,56 @@ public class ItemRepositoryTest
     [Fact]
     public void ReadItem()
     {
+        //Arrange
         var options = new DbContextOptionsBuilder<AppDbContext>()
             .UseInMemoryDatabase(databaseName: "ItemDatabase")
             .Options;
-
-        // Use a clean instance of the context to run the test
+        
         using (var context = new AppDbContext(options))
         {
             context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
             context.ItemTable.Add(new Item(){Id = 1, Name = "Item1", Quantity = 1});
             context.SaveChanges();
-            
+
             IItemRepository repo = new ItemRepository(context);
+            
+            //Act
             Item item = repo.Read(1);
 
+            //Assert
             Assert.Equal("Item1", item.Name);
+        }
+    }
+
+    [Fact]
+    public void DeleteItem()
+    {
+        //Arrange
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "ItemDatabase")
+            .Options;
+
+        using (var context = new AppDbContext(options))
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            Item item1 = new Item() { Id = 1, Name = "Item1", Quantity = 1 };
+            Item item2 = new Item() { Id = 2, Name = "Item2", Quantity = 2 };
+            context.ItemTable.Add(item1);
+            context.ItemTable.Add(item2);
+            context.SaveChanges();
+
+            IItemRepository repo = new ItemRepository(context);
+
+            //Act
+            Item item = repo.Delete(1);
+            List<Item> items = repo.ReadAll();
+
+            //Assert
+            Assert.Equal("Item1", item.Name);
+            Assert.Contains(item2, items);
+            Assert.DoesNotContain(item1, items);
         }
     }
 }
