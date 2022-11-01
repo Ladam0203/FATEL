fa7f36adf54d28408ebcf3a73888061eb1d1433b
Miloš Jozek <90444097+BoldHeadBall@users.noreply.github.com>
2022-11-01 11:09:41 +0100

    Finished postItemDTOValidator and first test for it

--- FATEL_Backend/Application/Validators/PostItemDTOValidator.cs
+++ FATEL_Backend/Application/Validators/PostItemDTOValidator.cs
@@ -8,12 +8,16 @@ public class PostItemDTOValidator : AbstractValidator<PostItemDTO>
 {
     public PostItemDTOValidator()
     {
-        //TODO: Implement validation
         RuleFor(item => item.Name).NotEmpty();
-        RuleFor(item => item.Lenght).GreaterThan(0);
-        RuleFor(item => item.Width).GreaterThan(0);
         //ask alex about if statement in fluent valid. 
-        //example => if unit == pieces, quantity greaterThan 0
+        //check lenght if unit == meter
         RuleFor(item => item.Lenght).GreaterThan(0).When(item => item.Unit == Unit.Meter);
+        //check quantity if unit == piece
+        RuleFor(item => item.Quantity).NotEmpty().When(item => item.Unit == Unit.Piece);
+        //check lenght and width if unit == square meter
+        RuleFor(item => item.Lenght).GreaterThan(0).When(item => item.Unit == Unit.SquareMeter);
+        RuleFor(item => item.Width).GreaterThan(0).When(item => item.Unit == Unit.SquareMeter);
+        //if note is not null then note cannot be empty
+        RuleFor(item => item.Note).NotEmpty().When(item => item.Note != null);
     }
 }
