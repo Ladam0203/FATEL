9620cca3fd05f2e4a4e6d0e192633fedae3a96cf
Ádám Lőrincz <adam7605@easv365.dk>
2022-11-01 13:38:02 +0100

    Tested and corrected the PostItemDTO Validator

--- FATEL_Backend/Domain/Item.cs
+++ FATEL_Backend/Domain/Item.cs
@@ -6,8 +6,8 @@ public class Item
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public float? Length { get; set; }
     public float? Width { get; set; }
-    public float? Lenght { get; set; }
     public Unit Unit { get; set; }
     public int Quantity { get; set; }
     public string? Note { get; set; }
