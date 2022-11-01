0c2bae7d2a73151cbd1ab94753c6e559558e6641
Ádám Lőrincz <adam7605@easv365.dk>
2022-11-01 12:01:56 +0100

    Working on PostItemDTOValidation - 2 tests not passing

--- FATEL_Backend/Application/DTOs/PostItemDTO.cs
+++ FATEL_Backend/Application/DTOs/PostItemDTO.cs
@@ -7,7 +7,7 @@ public class PostItemDTO
 
     public string Name { get; set; }
     public float?  Width { get; set; }
-    public float? Lenght { get; set; }
+    public float? Length { get; set; }
     public Unit Unit { get; set; }
     public int Quantity { get; set; }
     public string? Note { get; set; }
--- FATEL_Backend/Application/Validators/PostItemDTOValidator.cs
+++ FATEL_Backend/Application/Validators/PostItemDTOValidator.cs
@@ -9,13 +9,18 @@ public class PostItemDTOValidator : AbstractValidator<PostItemDTO>
     public PostItemDTOValidator()
     {
         RuleFor(item => item.Name).NotEmpty();
-        //ask alex about if statement in fluent valid. 
-        //check lenght if unit == meter
-        RuleFor(item => item.Lenght).GreaterThan(0).When(item => item.Unit == Unit.Meter);
-        //check quantity if unit == piece
-        RuleFor(item => item.Quantity).NotEmpty().When(item => item.Unit == Unit.Piece);
-        //check lenght and width if unit == square meter
-        RuleFor(item => item.Lenght).GreaterThan(0).When(item => item.Unit == Unit.SquareMeter);
+        RuleFor(item => item.Unit).NotEmpty();
+
+        //Quantity
+        RuleFor(item => item.Quantity).GreaterThan(0);
+        //Piece Unit
+        RuleFor(item => item.Length).Empty().When(item => item.Unit == Unit.Piece);
+        RuleFor(item => item.Width).Empty().When(item => item.Unit == Unit.Piece);
+        //Meter Unit
+        RuleFor(item => item.Length).GreaterThan(0).When(item => item.Unit == Unit.Meter);
+        RuleFor(item => item.Width).Empty().When(item => item.Unit == Unit.Meter);
+        //SquareMeter Unit
+        RuleFor(item => item.Length).GreaterThan(0).When(item => item.Unit == Unit.SquareMeter);
         RuleFor(item => item.Width).GreaterThan(0).When(item => item.Unit == Unit.SquareMeter);
         //if note is not null then note cannot be empty
         RuleFor(item => item.Note).NotEmpty().When(item => item.Note != null);
