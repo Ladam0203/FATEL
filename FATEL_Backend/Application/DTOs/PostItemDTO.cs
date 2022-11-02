cb84a21dc235acb8bf2bd072ca311202ab4f98ab
Ádám Lőrincz <adam7605@easv365.dk>
2022-11-02 10:17:03 +0100

    Added TODOs

--- FATEL_Backend/Application/DTOs/PostItemDTO.cs
+++ FATEL_Backend/Application/DTOs/PostItemDTO.cs
@@ -8,6 +8,6 @@ public class PostItemDTO
     public float? Length { get; set; }
     public float?  Width { get; set; }
     public Unit Unit { get; set; }
-    public int Quantity { get; set; }
+    public int Quantity { get; set; } //TODO: This will have to be removed as soon as we have a diary/movement manager
     public string? Note { get; set; }
 }
