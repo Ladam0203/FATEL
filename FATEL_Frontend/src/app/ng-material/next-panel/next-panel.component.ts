6209886078f7c977148942da2a260caa58738f0c
RomanMeasv <90683512+RomanMeasv@users.noreply.github.com>
2022-11-03 12:00:21 +0100

    Accordion WIP

---
+++ FATEL_Frontend/src/app/ng-material/next-panel/next-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-next-panel',
+  templateUrl: './next-panel.component.html',
+  styleUrls: ['./next-panel.component.css']
+})
+export class NextPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
