c9704b5a6976d4c51c178838dc9101b4b04867f4
RomanMeasv <90683512+RomanMeasv@users.noreply.github.com>
2022-11-03 14:35:06 +0100

    categoriseItems function WIP

--- FATEL_Frontend/src/app/inventory/inventory.component.ts
+++ FATEL_Frontend/src/app/inventory/inventory.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Item} from "../entities/item";
 import {ItemService} from "../services/item.service";
 import {ITEMS} from "../mock-objects/mock-items";
+import {Category} from "../entities/category";
 
 @Component({
   selector: 'inventory',
@@ -10,6 +11,9 @@ import {ITEMS} from "../mock-objects/mock-items";
 })
 export class InventoryComponent implements OnInit {
   items: Item[] = [];
+  categories: Category[] = [];
+
+  secondPanelOpenState: boolean = false;
 
   constructor(private itemService: ItemService) { }
 
@@ -22,6 +26,32 @@ export class InventoryComponent implements OnInit {
     this.itemService.readAll()
       .subscribe(items => {
         this.items = items;
+        this.categoriseItems();
       });
+  }
+
+  private categoriseItems(): void {
+    //TODO: Create categorizing logic by name
+    /*const names: string[] = [];
+    for(const item of this.items){
+      if(!names.includes(item.name))
+        names.push(item.name);
+    }
+    console.log(names);*/
+
+    const categories: Category[] = [];
+    for(const item of this.items){
+      if(!categories.map(value => {
+        return value.name
+      }).includes(item.name))
+        categories.push({name: item.name, items: [item]})
+      else {
+        // @ts-ignore
+        categories.filter(value => {
+          return value.name == item.name;
+        }).at(0).items.push(item);
+      }
+    }
+    console.log(categories);
   }
 }
