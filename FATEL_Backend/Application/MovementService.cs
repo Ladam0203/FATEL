34bf1ba1345d76bdec28ea5b28abd6308a50d8a0
Ádám Lőrincz <adam7605@easv365.dk>
2022-11-05 09:34:13 +0100

    Fixed validator issue

--- FATEL_Backend/Application/MovementService.cs
+++ FATEL_Backend/Application/MovementService.cs
@@ -12,11 +12,11 @@ public class MovementService : IMovementService
     private readonly IMovementRepository _movementRepository;
     private readonly IValidator<Movement> _movementValidator;
 
-    public MovementService(IItemRepository itemRepository, IMovementRepository movementRepository, IValidator<Movement> _movementValidator)
+    public MovementService(IItemRepository itemRepository, IMovementRepository movementRepository, IValidator<Movement> movementValidator)
     {
         _itemRepository = itemRepository;
         _movementRepository = movementRepository;
-        _movementValidator = _movementValidator;
+        _movementValidator = movementValidator;
     }
 
     public Entry Record(Movement movement)
@@ -28,10 +28,11 @@ public class MovementService : IMovementService
         //Check if item with id exists, if not, this will throw a KeyNotFoundException
         Item item = _itemRepository.Read(movement.Item.Id);
         
+        
         //Check if stored item is the same as the one in the movement
         if (JsonConvert.SerializeObject(item) != JsonConvert.SerializeObject(movement.Item)) 
             throw new ValidationException("Item in body does not match with stored Item");
-        
+
         //Change the item based on the movement
         item.Quantity += movement.Change;
         //Create a diary entry
