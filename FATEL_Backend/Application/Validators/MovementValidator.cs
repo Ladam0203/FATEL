4386fb5dc60849057bfd5f6c65c33eda89d025b1
Ádám Lőrincz <adam7605@easv365.dk>
2022-11-05 09:27:00 +0100

    Added Movement validation and checking whether the object stored is same as in the request

---
+++ FATEL_Backend/Application/Validators/MovementValidator.cs
@@ -0,0 +1,13 @@
+using Domain;
+using FluentValidation;
+
+namespace Application.Validators;
+
+public class MovementValidator : AbstractValidator<Movement>
+{
+    public MovementValidator()
+    {
+        RuleFor(movement => movement.Item.Id).GreaterThan(0).WithMessage("Item id is in invalid range");
+        RuleFor(movement => movement.Change).NotEqual(0).WithMessage("Change cannot be 0");
+    }
+}
