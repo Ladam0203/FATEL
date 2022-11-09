ba2eb043c76be6347a62bc55ccdd034b1d7519d3
Ádám Lőrincz <adam7605@easv365.dk>
2022-11-09 10:46:30 +0100

    Added TODO

--- FATEL_Backend/Application/ItemService.cs
+++ FATEL_Backend/Application/ItemService.cs
@@ -87,7 +87,7 @@ public class ItemService : IItemService
 
         //Check if stored item is the same as the one in the movement
         if (JsonConvert.SerializeObject(item) != JsonConvert.SerializeObject(movement.Item)) 
-            throw new ValidationException("Item in body does not match with stored Item");
+            throw new ValidationException("Item in body does not match with stored Item"); //TODO: This most probably should be a different exception
 
         //Change the item based on the movement
         item.Quantity += movement.Change;
