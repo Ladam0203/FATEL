60477e87085a16d64662e30611f1b2cd3ce93298
Ádám Lőrincz <adam7605@easv365.dk>
2022-11-11 14:25:31 +0100

    Update requiredIfMatches.ts

--- FATEL_Frontend/src/app/validators/requiredIfMatches.ts
+++ FATEL_Frontend/src/app/validators/requiredIfMatches.ts
@@ -4,7 +4,7 @@
 
 import {AbstractControl, ValidatorFn} from '@angular/forms';
 
-export function requiredIfMatches(expectedValue: any, actualValue: any): ValidatorFn {
+export function requiredIfMatches(actualValue: any, expectedValue: any): ValidatorFn {
   return (control: AbstractControl): {[key: string]: any} | null => {
     const actual = control.parent?.get(actualValue)?.value;
     const forbidden = actual === expectedValue && control.value === '';
