51d7881f89554902eec76cb2c35f2a40f5406cd8
RomanMeasv <90683512+RomanMeasv@users.noreply.github.com>
2022-11-13 17:18:35 +0100

    removed unused things

--- FATEL_Frontend/src/app/app.module.ts
+++ FATEL_Frontend/src/app/app.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { WarehouseComponent } from './warehouse/warehouse.component';
 import { InventoryComponent } from './inventory/inventory.component';
-import { DiaryComponent } from './diary/diary.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -27,9 +25,7 @@ import { MatSidenavContainerComponent } from './mat-sidenav-container/mat-sidena
 @NgModule({
   declarations: [
     AppComponent,
-    WarehouseComponent,
     InventoryComponent,
-    DiaryComponent,
     KeysPipe,
     AddItemComponent,
     FilterPipe,
