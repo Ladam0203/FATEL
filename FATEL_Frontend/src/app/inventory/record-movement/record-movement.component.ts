5923981034e594b250d4633a7e39c2a3af71efa8
Ádám Lőrincz <adam7605@easv365.dk>
2022-11-19 12:28:16 +0100

    Basic setup for movement (not showing up lol)

--- FATEL_Frontend/src/app/inventory/record-movement/record-movement.component.ts
+++ FATEL_Frontend/src/app/inventory/record-movement/record-movement.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {greaterThanDirective} from "../../validators/greaterThan.directive";
+import {ItemService} from "../../services/item.service";
+import {Store} from "@ngrx/store";
+import {Item} from "../../entities/item";
 
 @Component({
   selector: 'app-record-movement',
@@ -7,8 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecordMovementComponent implements OnInit {
 
-  constructor() { }
+  constructor(private itemService: ItemService, private readonly store: Store<any>) {
+  }
+
+  movementForm: FormGroup = new FormGroup({
+    change: new FormControl(),
+  });
+
+  categoriesState = this.store.select('categoriesState');
+
+  itemToRecordMovementOn: Item | undefined;
 
   ngOnInit(): void {
+    this.movementForm = new FormGroup({
+      change: new FormControl('', [
+        Validators.required,
+        greaterThanDirective()
+     ])
+    });
   }
 }
