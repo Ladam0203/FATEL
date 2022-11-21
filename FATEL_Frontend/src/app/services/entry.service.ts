4469fe93936a3b205c1b80b59a77fe8a4700d807
Ádám Lőrincz <adam7605@easv365.dk>
2022-11-21 12:05:21 +0100

    Removed unused imports

--- FATEL_Frontend/src/app/services/entry.service.ts
+++ FATEL_Frontend/src/app/services/entry.service.ts
@@ -1,11 +1,7 @@
 import {Injectable} from '@angular/core';
-import {catchError} from 'rxjs/operators';
-import {Item} from "../entities/item";
 import {HttpClient} from "@angular/common/http";
 import axios from "axios";
-import {PostItemDTO} from "../entities/DTOs/PostItemDTO";
 import {MatSnackBar} from "@angular/material/snack-bar";
-import {Movement} from "../entities/DTOs/Movement";
 import {Entry} from "../entities/entry";
 
 export const customAxios = axios.create({
