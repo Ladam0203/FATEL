ee78262d670c9bf6df3a177cd5b49564c4a3a795
RomanMeasv <90683512+RomanMeasv@users.noreply.github.com>
2022-11-22 16:53:42 +0100

    Diary WIP

--- FATEL_Frontend/src/app/diary/diary.component.ts
+++ FATEL_Frontend/src/app/diary/diary.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Entry} from "../entities/entry";
 import {EntryService} from "../services/entry.service";
 
 @Component({
-  selector: 'app-diary',
+  selector: 'diary',
   templateUrl: './diary.component.html',
   styleUrls: ['./diary.component.css']
 })
 export class DiaryComponent implements OnInit {
 
+  displayedColumns: string[] = ['timestamp', 'itemName', 'change', 'quantityAfterChange'];
+
   entries: Entry[] = [];
 
-  constructor(private entryService: EntryService) { }
+  constructor(private entryService: EntryService) {
+  }
 
   ngOnInit(): void {
     this.entryService.readAll()
-      .then(entries => this.entries = entries);
+      .then(entries => {
+        this.entries = entries;
+        console.log(entries);
+      });
   }
 
 }
