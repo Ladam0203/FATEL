2648db97572a70e2ea7ccdff9041d1f6bc7fc849
Ádám Lőrincz <adam7605@easv365.dk>
2022-11-22 17:00:49 +0100

    Entries filter

--- FATEL_Frontend/src/app/diary/diary.component.ts
+++ FATEL_Frontend/src/app/diary/diary.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Entry} from "../entities/entry";
 import {EntryService} from "../services/entry.service";
+import {selectSearchbarQueryValue} from "../inventory/states/filter-bar.actions";
+import {Store} from "@ngrx/store";
 
 @Component({
   selector: 'diary',
@@ -12,8 +14,9 @@ export class DiaryComponent implements OnInit {
   displayedColumns: string[] = ['timestamp', 'itemName', 'change', 'quantityAfterChange'];
 
   entries: Entry[] = [];
+  searchbarQuery = this.store.select(selectSearchbarQueryValue);
 
-  constructor(private entryService: EntryService) {
+  constructor(private entryService: EntryService, private readonly store: Store<any>) {
   }
 
   ngOnInit(): void {
