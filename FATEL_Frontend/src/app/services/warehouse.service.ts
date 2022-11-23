6e09130c1d6d203f416fd2d54fc780ec97a0f986
Ádám Lőrincz <adam7605@easv365.dk>
2022-11-23 09:57:19 +0100

    Warehouse service with axios and readAll method

--- FATEL_Frontend/src/app/services/warehouse.service.ts
+++ FATEL_Frontend/src/app/services/warehouse.service.ts
@@ -1,48 +1,21 @@
 import {Injectable} from '@angular/core';
 import {Warehouse} from '../entities/warehouse'
-import {Observable, of} from 'rxjs';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {catchError, map, tap} from 'rxjs/operators';
+import axios from "axios";
+
+export const customAxios = axios.create({
+  baseURL: 'http://localhost:5175/api/warehouse/',
+})
 
 @Injectable({
   providedIn: 'root'
 })
 export class WarehouseService {
-  private warehouseUrl = 'api/warehouse'; //URL to web api
-
-  httpOptions = {
-    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  constructor() {
   }
 
-  constructor(private http: HttpClient) {
-
-  }
-  /*
-  getAllWarehouses(): Observable<Warehouse[]> {
-    return this.http.get<Warehouse[]>(this.warehouseUrl)
-      .pipe(
-        tap(_ => this.log('fetched warehouses')),
-        catchError(this.handleError<Warehouse[]>('getAllWarehouses', []))
-      );
-  }*/
-
-  private handleError<T>(operation = 'operation', result?: T){
-    return (error: any): Observable<T> => {
-      console.error(error);
-      this.log(`${operation} failed: ${error.message}`);
-      return of(result as T);
-    }
-  }
-
-  getWarehouse(id: number): Observable<Warehouse> {
-    const url = `${this.warehouseUrl}/${id}`;
-    return this.http.get<Warehouse>(url).pipe(
-      tap(_ => this.log(`fetched warehouse id=${id}`)),
-      catchError(this.handleError<Warehouse>(`getWarehouse id=${id}`))
-    )
-  }
-
-  private log(message: string){
-    console.log(message);
+  async readAll(): Promise<Warehouse[]> {
+    const response = await customAxios.get<Warehouse[]>('readall');
+    return response.data;
   }
 }
+
