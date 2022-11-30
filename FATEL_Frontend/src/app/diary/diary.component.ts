3ce5e393a8c7a0aa2f45fc877a739bb9df36be18
Miloš Jozek <dumm0232@easv365.dk>
2022-11-30 10:52:56 +0100

    diary repo creation

--- FATEL_Frontend/src/app/diary/diary.component.ts
+++ FATEL_Frontend/src/app/diary/diary.component.ts
@@ -3,6 +3,8 @@ import {Entry} from "../entities/entry";
 import {EntryService} from "../services/entry.service";
 import {selectSearchbarQueryValue} from "../states/filter-bar.actions";
 import {Store} from "@ngrx/store";
+import {Warehouse} from "../entities/warehouse";
+import {ReportService} from "../services/report/report.service";
 
 @Component({
   selector: 'diary',
@@ -22,7 +24,10 @@ export class DiaryComponent implements OnInit {
 
   name: string = 'Warehouse';
 
-  constructor(private entryService: EntryService, private readonly store: Store<any>) {
+  warehouse: Warehouse | undefined;
+  searchBarQuery: String | undefined;
+
+  constructor(private entryService: EntryService, private readonly store: Store<any>, private readonly reportService: ReportService) {
   }
 
   ngOnInit(): void {
@@ -30,6 +35,17 @@ export class DiaryComponent implements OnInit {
       this.name = state.selectedWarehouse.name;
       this.entries = state.selectedWarehouse.diary;
       this.entries = [...this.entries].sort((a, b) => Number(new Date(a.timestamp)) - Number(new Date(b.timestamp))).reverse()
+      this.warehouse = state.selectedWarehouse;
     })
+  }
+
+  exportPDF() {
+    if (!this.warehouse) {
+      return;
+    }
+    let filtered = this.warehouse.diary.filter(entry =>
+      entry.itemName.toLowerCase().indexOf(this.searchBarQuery?.toLowerCase() || '') != -1);
+    console.log(filtered);
+    this.reportService.createDiaryReport(this.warehouse.name, ['Timestamp', 'Item Name', 'Change', 'After'], this.warehouse.diary)
   }
 }
--- FATEL_Frontend/src/app/services/report/report.service.ts
+++ FATEL_Frontend/src/app/services/report/report.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import jsPDF from "jspdf";
 import {RobotoRegular} from "./Roboto-Regular";
 import autoTable from "jspdf-autotable";
 import {Unit} from "../../entities/units";
+import {Entry} from "../../entities/entry";
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,8 @@ import {Unit} from "../../entities/units";
 export class ReportService {
   units: typeof Unit = Unit;
 
-  constructor() { }
+  constructor() {
+  }
 
   createReport(header: string, fields: string[], data: any[]) {
     const doc = new jsPDF('p', 'pt', 'a4');
@@ -26,8 +28,7 @@ export class ReportService {
       lowercaseFields.forEach((lF) => {
         if (lF === 'unit') {
           row.push(this.units[item[lF]]);
-        }
-        else {
+        } else {
           row.push(item[lF]);
         }
       });
@@ -45,6 +46,34 @@ export class ReportService {
         theme: "plain",
       }
     );
-    doc.save(header+'.pdf');
+    doc.save(header + '.pdf');
+  }
+
+  createDiaryReport(header: string, fields: string[], data: Entry[]) {
+    const doc = new jsPDF('p', 'pt', 'a4');
+
+    doc.addFileToVFS('Roboto-Regular.ttf', RobotoRegular);
+    doc.addFont('Roboto-Regular.ttf', 'Roboto-Regular', 'regular');
+    let lowercaseFields = fields.map(f => f.toLowerCase());
+
+    let parsedData: any[][] = [];
+    data.forEach((entry) => {
+      let row: any[] = [new Date(entry.timestamp).toLocaleDateString('hu-HU') + " " + new Date(entry.timestamp).toLocaleTimeString('hu-HU').slice(0, 5), entry.itemName, Number(entry.change.toFixed(3)), Number(entry.quantityAfterChange.toFixed(3))];
+      parsedData.push(row);
+    });
+    console.log(parsedData);
+
+
+    doc.text("Diary " + header + " " + new Date().toLocaleDateString(), 20, 25);
+    autoTable(doc, {
+        head: [fields],
+        body: parsedData,
+        styles: {
+          font: 'Roboto-Regular',
+        },
+        theme: "striped",
+      }
+    );
+    doc.save(header + '.pdf');
   }
 }
