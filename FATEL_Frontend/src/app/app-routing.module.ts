a809147c29f88ea5daa42cae83abbd9e1d17610b
ChristianHesberg <chri59kx@easv365.dk>
2022-12-02 09:51:03 +0100

    login done

--- FATEL_Frontend/src/app/app-routing.module.ts
+++ FATEL_Frontend/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {FatelComponent} from "./fatel/fatel.component";
+import {AuthguardService} from "./services/authguard.service";
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: '', component: FatelComponent},
+  {path: '', component: FatelComponent, canActivate: [AuthguardService]},
 ]
 
 @NgModule({
