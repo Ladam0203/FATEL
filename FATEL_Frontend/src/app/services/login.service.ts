3d6648eb3e6c1f3950643bf2f18b79a3dbf398c7
ChristianHesberg <chri59kx@easv365.dk>
2022-12-02 11:04:28 +0100

    notify user on error
    -notify user of incorrect username or password.
    -notify user of fail to connect to server

--- FATEL_Frontend/src/app/services/login.service.ts
+++ FATEL_Frontend/src/app/services/login.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import {Login} from "../entities/DTOs/login";
 import axios from "axios";
 import {environment} from "../../environments/environment";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {catchError} from "rxjs/operators";
 
 export const customAxios = axios.create({
   baseURL: environment.baseUrl
@@ -12,7 +14,27 @@ export const customAxios = axios.create({
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private matSnackBar: MatSnackBar) {
+    customAxios.interceptors.response.use(
+      response => {
+        return response;
+      },
+      rejected =>{
+        if(!rejected.response)
+        {
+          this.matSnackBar.open("Could not connect to server",
+            undefined,
+            {duration: 4000});
+        }
+        else if(rejected.response.status == 401){
+          this.matSnackBar.open("Incorrect username or password",
+            undefined,
+            {duration: 4000});
+        }
+        catchError(rejected);
+      }
+    )
+  }
 
   async login(dto: Login){
     try {
