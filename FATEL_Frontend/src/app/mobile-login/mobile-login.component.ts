6961a29c35bd9b3a4bff3271a8791985044de161
ChristianHesberg <chri59kx@easv365.dk>
2022-12-06 11:43:09 +0100

    basic mobile login

--- FATEL_Frontend/src/app/mobile-login/mobile-login.component.ts
+++ FATEL_Frontend/src/app/mobile-login/mobile-login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Login} from "../entities/DTOs/login";
+import {LoginService} from "../services/login.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-mobile-login',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MobileLoginComponent implements OnInit {
 
-  constructor() { }
+  username: string = "";
+  password: string = "";
+  unauthorized: boolean = true;
+
+  hide: boolean = true;
+
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  async login() {
+    let loginDto: Login = {
+      username: this.username,
+      password: this.password
+    };
+    const token = await this.loginService.login(loginDto);
+    if (!token) {
+      this.unauthorized = token;
+    } else {
+      localStorage.setItem('token', token);
+      await this.router.navigate(['mobile']);
+    }
+
+  }
 }
