cb0cdadd1b5e2427d4306b1a389120e590ad086b
ChristianHesberg <chri59kx@easv365.dk>
2022-12-14 10:29:44 +0100

    fixed the bug

--- FATEL_Backend/Application/Interfaces/IItemRepository.cs
+++ FATEL_Backend/Application/Interfaces/IItemRepository.cs
@@ -8,6 +8,7 @@ public interface IItemRepository
     Item Read(int id);
     List<Item> ReadAll();
     Item Update(Item item);
+    Item UpdateQuantity(Item item);
     List<Item> UpdateNameRange(List<Item> items);
     Item Delete(int id);
     double ReadTotalQuantityOf(Item item);
